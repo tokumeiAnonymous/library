a029ef80ec326709ef428c1f66e1709896ff52be
Jerome Tokumei <jerometokumei@gmail.com>
2022-01-31 22:38:57 +0800

    Added table and pop-up form

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 
-function Book() {
+function Book(title, author, pages, read) {
 
-    let title, author, pages, status;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    
+}
 
-    constructor(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+function generateTable(body, data){
+    for (let element of data){
+        let row = body.insertRow();
+
+        for(key in element){
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+
+        }
+        addButton(row);
+
     }
 }
 
-let myLib =[];
+function addButton(row) {
+
+    let cell = row.insertCell();
+    const btn = document.createElement('button');
+    btn.onclick = deleteEntry;
+    btn.innerText = "Delete";
+    cell.appendChild(btn);
+}
+
+function deleteEntry() {
+    body.removeChild(body.lastElementChild);
+}
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
+
+let myLib = [];
 
 let book1 = new Book("The Book Thief",
             "Markus Zusak", 400, true);
-let book2 = new Book ("The Idiot", 
-            "Fyodor Doestoyevsky", 400, true);
-let book3 = new Book ("The Name of The Wind", 
-            "Patrick Rothfuss", 600, false);
+
+let book2 = new Book("The Idiot", 
+            "Fyodor Doestoyevsky", 655, true);
+
+let book3 = new Book("The Name of The Wind", 
+            "Patrick Rothfuss", 719, false);
 
 myLib.push(book1);
 myLib.push(book2);
 myLib.push(book3);
+
+const body = document.querySelector('.table-body');
+
+generateTable(body, myLib);
+
+const addBook = document.querySelector('.add-book');
+addBook.addEventListener('click', openForm);
