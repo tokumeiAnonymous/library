9b8e2754aad32e5a6d0ace46230552887b635a2d
Jerome Tokumei <jerometokumei@gmail.com>
2022-02-03 00:08:24 +0800

    Changed to factory functions

--- script.js
+++ script.js
@@ -1,101 +1,3 @@
-function Book(title, author, pages, read) {
-
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    
-}
-
-function updateTable() {
-
-    body.textContent = '';
-
-    for (let book in myLib){
-        let row = document.createElement('tr');
-        
-    }
-}
-
-function addReadBtn(book) {
-
-    let readStatusTd = document.createElement('td');
-    let readStatusBtn = document.createElement('button');
-    readStatusBtn.onclick = changeStatus;
-    readStatusBtn.textContent = "Change Status";
-    readStatusTd.appendChild(readStatusBtn);
-
-    return readStatusTd;
-}
-
-function changeStatus(book) {
-    book.read = !book.read;
-    updateTable();
-}
-
-function addDeleteBtn(index) {
-
-    let deleteTd = document.createElement('td');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.onclick = deleteEntry;
-    deleteBtn.textContent = "Delete";
-    cell.appendChild(deleteBtn);
-
-    return deleteTd;
-}
-
-function deleteEntry() {
-    myLibrary.splice(index, 1);
-    updateTable();
-}
-
-function openForm() {
-    document.getElementById("popupForm").style.display = "block";
-}
-
-function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
-    clearForm();
-}
-
-function submitData() {
-    addBookToLib();
-    updateTable();
-    closeForm();
-}
-
-function addBookTolib() {
-    let title = titleInput.value;
-    let author = authorInput.value;
-    let page = pageInput.value;
-    let readStatus = read.value;
-    let newBook = new Book(title, author, page, read);
-    myLib.push(newBook);
-    console.log("Book added");
-}
-
-function clearForm() {
-
-    titleInput.value = "";
-    authorInput.value = "";
-    pageInput.value = "";
-}
-
-let myLib = [];
-
-let book1 = new Book("The Book Thief",
-            "Markus Zusak", 400, "Read");
-
-let book2 = new Book("The Idiot", 
-            "Fyodor Doestoyevsky", 655, "Read");
-
-let book3 = new Book("The Name of The Wind", 
-            "Patrick Rothfuss", 719, "Not Read");
-
-myLib.push(book1);
-myLib.push(book2);
-myLib.push(book3);
-
 
 // DOM Objects
 const body = document.querySelector('.table-body');
@@ -106,30 +8,107 @@ const authorInput = form.querySelector('#author');
 const pageInput = form.querySelector('#pages');
 const read = form.querySelector('#read');
 const submit = form.querySelector('#submit');
+const cancel = form.querySelector('#cancel');
 
-const removeFromLibrary = (index) => {
+addBook.addEventListener('click', () => {
+    document.getElementById("popupForm").style.display = "block";
+});
+cancel.addEventListener('click', () => {
+    document.getElementById("popupForm").style.display = "none";
+    clearForm();
+});
 
+submit.addEventListener('click', () => {
+
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let page = pageInput.value;
+    let readStatus = read.value;
+    let newBook = book(title, author, page, read);
+    myLib.push(newBook);
+});
+
+
+const book = (title, author, pages, read) => {
+    
+    const changeRead = (read) => {
+        if (read == "Read") this.read = "Not Read";
+        else this.read = "Read";
+    }
+
+    return {title, author, pages, read, changeRead}
 }
 
-const createDeleteTd = (index) => {
+const clearForm = () => {
 
+    titleInput.value = "";
+    authorInput.value = "";
+    pageInput.value = "";
 }
-/*
+
+const createReadTd = (book) => {
+
+    let readStatusTd = document.createElement('td');
+    let readStatusBtn = document.createElement('button');
+    readStatusBtn.textContent = book.read.value;
+    readStatusBtn.addEventListener('click', () => {
+        book.changeRead(book.read)
+    });
+    readStatusTd.appendChild(readStatusTd);
+
+    return readStatusTd;
+}
+
+const createDeleteTd = (book) => {
+
+    let deleteTd = document.createElement('td');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener('click', () => {
+        myLib.splice(myLib.indexOf(book), 1)
+    });
+    deleteTd.appendChild(deleteBtn);
+
+    return deleteTd;
+}
+
 const updateTable = () => {
+
     body.textContent = '';
 
-    myLib.forEach((book, index)) => {
+    myLib.forEach((book, index) => {
         let row = document.createElement('tr');
         Object.keys(book).forEach(prop => {
             let newTd = document.createElement('td');
             newTd.textContent = book[prop];
             row.appendChild(newTd);
-        })
-    }
+        });
+    
+        row.appendChild(createReadTd(book));
+        row.appendChild(createDeleteTd(book));
+    });
+    body.appendChild(row);
 }
-*/
 
+const submitData = () => {
+    addBookTolib();
+    updateTable();
+    closeForm();
+}
 
-updateTable(body, myLib);
+let myLib = [];
 
-addBook.addEventListener('click', openForm);
+let book1 = book("The Book Thief",
+            "Markus Zusak", 400, "Read");
+
+let book2 = book("The Idiot", 
+            "Fyodor Doestoyevsky", 655, "Read");
+
+let book3 = book("The Name of The Wind", 
+            "Patrick Rothfuss", 719, "Not Read");
+
+myLib.push(book1);
+myLib.push(book2);
+myLib.push(book3);
+
+updateTable();
